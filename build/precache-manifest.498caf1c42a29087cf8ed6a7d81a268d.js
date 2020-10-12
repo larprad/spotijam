149bcb36df281113337ed6bc7bcc9f62ae22345a
larprad <59915248+larprad@users.noreply.github.com>
2020-10-12 17:37:12 +0200

    Correct build minor bug

---
+++ build/precache-manifest.498caf1c42a29087cf8ed6a7d81a268d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e809463af0502e643c55789d4fbb0022",
+    "url": "/spotijam/index.html"
+  },
+  {
+    "revision": "1d66ddc60e521e846917",
+    "url": "/spotijam/static/css/main.5bf6d873.chunk.css"
+  },
+  {
+    "revision": "0de05e1d583423ac5de9",
+    "url": "/spotijam/static/js/2.a469b212.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/spotijam/static/js/2.a469b212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d66ddc60e521e846917",
+    "url": "/spotijam/static/js/main.f2bb34e9.chunk.js"
+  },
+  {
+    "revision": "30fa969f67eab3b64762",
+    "url": "/spotijam/static/js/runtime-main.8797f214.js"
+  },
+  {
+    "revision": "2fa07257afae8e5cc1f3a798dfb98d1d",
+    "url": "/spotijam/static/media/bg.2fa07257.svg"
+  },
+  {
+    "revision": "665926e22b76aa6339da4f7eab12c067",
+    "url": "/spotijam/static/media/delete.665926e2.svg"
+  },
+  {
+    "revision": "fd6ce3299fa2f33db40a4a8528ae6458",
+    "url": "/spotijam/static/media/dino.fd6ce329.png"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/spotijam/static/media/edit.2927f8de.svg"
+  },
+  {
+    "revision": "899514fa9a6ea88eb8dc3046cb83ded2",
+    "url": "/spotijam/static/media/refresh.899514fa.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a1d961db29fb71848fecc4bd6724a27.js"
+  "/spotijam/precache-manifest.498caf1c42a29087cf8ed6a7d81a268d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spotijam/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
