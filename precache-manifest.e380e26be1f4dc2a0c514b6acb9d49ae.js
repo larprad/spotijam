a3179d6d609e85494ac65f887144c5f5c44518a7
larprad <59915248+larprad@users.noreply.github.com>
2020-10-12 17:11:30 +0200

    Make build

---
+++ precache-manifest.e380e26be1f4dc2a0c514b6acb9d49ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a4e8f3972d29479ece9d4f71b69d87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88fee604400b9bb573fc",
+    "url": "/static/css/main.c8aba4e8.chunk.css"
+  },
+  {
+    "revision": "692fb23af61a91586cfd",
+    "url": "/static/js/2.3a899f76.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.3a899f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88fee604400b9bb573fc",
+    "url": "/static/js/main.1b314052.chunk.js"
+  },
+  {
+    "revision": "fb56df1272f7b316e406",
+    "url": "/static/js/runtime-main.9372aaee.js"
+  },
+  {
+    "revision": "2fa07257afae8e5cc1f3a798dfb98d1d",
+    "url": "/static/media/bg.2fa07257.svg"
+  },
+  {
+    "revision": "665926e22b76aa6339da4f7eab12c067",
+    "url": "/static/media/delete.665926e2.svg"
+  },
+  {
+    "revision": "fd6ce3299fa2f33db40a4a8528ae6458",
+    "url": "/static/media/dino.fd6ce329.png"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/static/media/edit.2927f8de.svg"
+  },
+  {
+    "revision": "899514fa9a6ea88eb8dc3046cb83ded2",
+    "url": "/static/media/refresh.899514fa.svg"
+  }
+]);
